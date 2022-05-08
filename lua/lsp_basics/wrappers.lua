632714bd3ab355eb6e725b5a78cd8730f12d14d2
Timothée Sterle <timothee-s@mailoo.org>
2022-05-08 14:48:18 +0200

    refactor: use vim.lsp.buf.format() when available

--- lua/lsp_basics/wrappers.lua
+++ lua/lsp_basics/wrappers.lua
@@ -1,19 +1,27 @@
 local M = {}
 local lsp = vim.lsp
 
+local formatting = lsp.buf.format and function()
+    lsp.buf.format({ async = true })
+end or lsp.buf.formatting
+
+local formatting_sync = lsp.buf.format and function()
+    lsp.buf.format({ async = false })
+end or lsp.buf.formatting_sync
+
 function M.format_command(range_given, line1, line2, bang)
     if range_given then
-        lsp.buf.range_formatting(nil, {line1, 0}, {line2, 99999999})
+        lsp.buf.range_formatting(nil, {line1, 0}, {line2, math.huge})
     elseif bang then
-        lsp.buf.formatting_sync()
+        formatting_sync()
     else
-        lsp.buf.formatting()
+        formatting()
     end
 end
 
 function M.code_action(range_given, line1, line2)
     if range_given then
-        lsp.buf.range_code_action(nil, {line1, 0}, {line2, 99999999})
+        lsp.buf.range_code_action(nil, {line1, 0}, {line2, math.huge})
     else
         lsp.buf.code_action()
     end
